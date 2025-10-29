c7030198c265ef8c6558dfbc31109691a0598add
Birgül Özhan <birgulozhan9@gmail.com>
2025-10-29 13:28:13 +0300

    posts array ile fake API verisi oluşturuldu

---
+++ src/data/posts.js
@@ -0,0 +1,23 @@
+export const posts = [
+  {
+    title: "Uludağ Pınarlar Gölünden Aşağı Gezinti",
+    image: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800",
+    date: "20 Ekim 2025",
+    category: "DOĞA",
+    readTime: "5 dk",
+  },
+  {
+    title: "Looper Paradox: Zamanda Sıkış Yolculuğu",
+    image: "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?w=800",
+    date: "21 Ekim 2025",
+    category: "MACERA",
+    readTime: "8 dk",
+  },
+  {
+    title: "Yalnız İnsanla Fiziksel Doğma Hissetmek",
+    image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=800",
+    date: "22 Ekim 2025",
+    category: "YAŞAM",
+    readTime: "6 dk",
+  },
+];
