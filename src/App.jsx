84a154451c11bddf26b9114d4bd76d7acf1b7470
Birgül Özhan <birgulozhan9@gmail.com>
2025-10-29 13:22:11 +0300

    App bileşeni React Router ile sarıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Route, Router, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import BlogDetail from "./Pages/BlogDetail"; // Yeni detay sayfası
 
 function App() {
-
   return (
-    <Navbar />
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
